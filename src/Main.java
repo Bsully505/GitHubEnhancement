66bc6d7c85019f3315ae5a142d2adf476ac1c579
Bryan Sullivan <Bsully505@yahoo.com>
2021-04-06 19:58:18 -0400

    added jframe and created gui

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
 public class Main {
+
+    public Main(){//ini
+        JFrame frame = new JFrame("GitHub Edhancements");
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);//setting width and height of the frame
+        JButton DarkLightBool = new JButton("TOGGLE DARKMODE");
+        frame.setVisible(true);
+    }
+    public static void main(String[] args){
+        Main Run = new Main();
+    }
 }
